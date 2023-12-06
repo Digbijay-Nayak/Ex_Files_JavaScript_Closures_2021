202c179ac15ecc395b26ac5cf67ff38067ff0f3c
Digbijay-Nayak <nayakdigbijay888@gmail.com>
2023-12-06 16:55:42 +0530

    small changes

--- Ch01/01_01/finish/js/script.js
+++ Ch01/01_01/finish/js/script.js
@@ -33,7 +33,7 @@ const activities = {
 let state = {};
 let category = 'all';
 let url = 'http://api.openweathermap.org/data/2.5/weather?q=';
-let apiKey = '6703d771e5c1b9b6097d1b1790b2101c'; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+let apiKey = 'APIKEY'; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
 function updateActivityList(event) {
   if (event !== undefined && event.target.classList.contains('selected')) {
     return true;
